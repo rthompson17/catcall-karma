42ec9c80a2508382878ab590e1fd7de2a8d14fa7
username <youremail@domain.com>
2022-03-19 21:15:48 -0400

    new images, new  HTML, modified CSS

--- js/app.js
+++ js/app.js
@@ -1,24 +1,54 @@
-    let playerImg = null;
+   
+   
+   
+   
+   
+   // ---------- PLAYER MOVEMENT ---------- //
+   
+   let playerImg = null;
     function init() {
         playerImg = document.getElementById("playerGirl");
         playerImg.style.position = "relative";
         playerImg.style.left = "200px";
-        playerImg.style.top = "300px";
+        playerImg.style.top = "400px";
     }
 
     function playerGo(e) {
-        var key_code = e.which || e.key; // modify to not use keyCode -- use KeyboardEvent
+        var key_code = e.which || e.key;
         switch (key_code) {
-                case 32 || Space: //Up arrow key
+                case 32 || Space: // "K" is "which key" 75 or event code "KeyK"
                 moveUp(); 
-                break;
+                
         }
     }
 
     function moveUp() {
-        playerImg.style.top = parseInt(playerImg.style.top) - 5 + "px";
+        playerImg.style.top = parseInt(playerImg.style.top) - 2 + "px";
     }
-    window.onload = init;
+    window.onload = init; // gameStart will init
+
+
+// GAME BOARD LIMITATION
+
+const boardLimit = document.getElementById("gameBoard");
+function checkGoal(){
+    let characterTop = parseInt(window.getComputedStyle("#playerGirl").getPropertyValue("top"));
+    if(characterTop<=0){
+        alert("Game over");
+    }
+}
+
+//setInterval(checkGoal, 1000);
 
 
 
+   // ---------- PLAYER COORDINATES and COLLISION ---------- //
+
+//    let playerCoord = document.querySelector('playerGirl');
+//    let rect = elem.getBoundingClientRect();
+//    for (var key in rect) {
+//      if(typeof rect[key] !== 'function') {
+//        let makeCatcall = // choose catcaller audio function ;
+//        // play catcaller audio ;
+//      }
+//    }
