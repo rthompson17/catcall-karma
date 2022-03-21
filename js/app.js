d762208d4326499b5aadbf1a5e27ba622a393a48
username <youremail@domain.com>
2022-03-21 15:33:58 -0400

    updated positioning of all objects, added subway

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
-   
-   
-   
-   
-   
+// VARIABLES
+// let playerImg = null;
+// const boardLimit = document.getElementById("gameBoard");
+
+
    // ---------- PLAYER MOVEMENT ---------- //
    
    let playerImg = null;
     function init() {
         playerImg = document.getElementById("playerGirl");
         playerImg.style.position = "relative";
-        playerImg.style.left = "200px";
-        playerImg.style.top = "400px";
+        playerImg.style.left = "160px";
+        playerImg.style.top = "435px";
     }
 
     function playerGo(e) {
@@ -23,8 +23,8 @@
     }
 
     function moveUp() {
-        if(playerImg.style.top > "-5px") {
-        playerImg.style.top = parseInt(playerImg.style.top) - 5 + "px";
+        if(playerImg.style.top > "-70px") {
+        playerImg.style.top = parseInt(playerImg.style.top) - 2 + "px";
         } 
         checkGoal();
         // can use this function to check on encounters with the catcallers
@@ -38,14 +38,36 @@ const boardLimit = document.getElementById("gameBoard");
 function checkGoal(){
     let characterTop = playerImg.style.top;
     console.log(characterTop);
-    if(characterTop<="-5px"){
+    if(characterTop<="-70px"){
         alert("Game over");
     } // can check "mental health" within this function
 }
 
 // setInterval(checkGoal, 10);
 
+   // ------------ CCOUNTDOWN TIMER ---------------- //
+   function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+let timerStart = function () {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
 
    // ---------- PLAYER COORDINATES and COLLISION ---------- //
 
