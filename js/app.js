6ec38eeac1ea64bb6c7524b74dcfc036b7763d24
username <youremail@domain.com>
2022-03-21 19:54:58 -0400

    added catcaller audio, working on timer

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@
 // let playerImg = null;
 // const boardLimit = document.getElementById("gameBoard");
 
+//AUDIO VARIABLES
+const whistle = new Audio("audio/wolf-whistle-14621.mp3");
+const working = new Audio("audio/are-you-working.wav");
+const talkToYou = new Audio("audio/can-i-talk-to-you.wav");
+const smile = new Audio("audio/smile-for-us.wav");
+// ^^ make this an array and have it randomly choose the sounds --- have an array that is silent
 
    // ---------- PLAYER MOVEMENT ---------- //
    
@@ -17,14 +23,13 @@
         var key_code = e.which || e.key;
         switch (key_code) {
                 case 32 || Space: // "K" is "which key" 75 or event code "KeyK"
-                moveUp(); 
-                
+                moveUp();                
         }
     }
 
     function moveUp() {
         if(playerImg.style.top > "-70px") {
-        playerImg.style.top = parseInt(playerImg.style.top) - 2 + "px";
+        playerImg.style.top = parseInt(playerImg.style.top) - 25 + "px";
         } 
         checkGoal();
         // can use this function to check on encounters with the catcallers
@@ -33,12 +38,30 @@
 
 
 // GAME BOARD LIMITATION
+// const wolfWhistle = new Audio("audio/wolf-whistle-14621.mp3");
+
+// CATCALLL TRIGGER POINTS in PX => 371, 285, 209, 125, 61 (but odd number!!)
 
 const boardLimit = document.getElementById("gameBoard");
 function checkGoal(){
     let characterTop = playerImg.style.top;
     console.log(characterTop);
-    if(characterTop<="-70px"){
+    if (characterTop==="371px") {
+        whistle.play("whistle");
+    } if (characterTop==="285px") {
+        working.play("working");
+    } if (characterTop==="209px") {
+        talkToYou.play("talkToYou");
+    } if (characterTop==="125px") {
+        smile.play("smile");
+    } if (characterTop==="61px") {
+        working.play("working");
+    }
+
+
+ 
+
+    else if(characterTop<="-70px"){
         alert("Game over");
     } // can check "mental health" within this function
 }
@@ -46,36 +69,72 @@ function checkGoal(){
 // setInterval(checkGoal, 10);
 
    // ------------ CCOUNTDOWN TIMER ---------------- //
-   function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
+//    function startTimer(duration, display) {
+//     var timer = duration, minutes, seconds;
+//     setInterval(function () {
+//         minutes = parseInt(timer / 60, 10);
+//         seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+//         minutes = minutes < 10 ? "0" + minutes : minutes;
+//         seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = minutes + ":" + seconds;
+//         display.textContent = minutes + ":" + seconds;
 
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
-}
+//         if (--timer < 0) {
+//             timer = duration;
+//         }
+//     }, 1000);
+// }
 
-let timerStart = function () {
-    var fiveMinutes = 60 * 5,
-        display = document.querySelector('#time');
-    startTimer(fiveMinutes, display);
-};
+// let timerStart = function () {
+//     var fiveMinutes = 60 * 5,
+//         display = document.querySelector('#time');
+//     startTimer(fiveMinutes, display); 
+// };  
+  
+//////////////////////
+var timerClock = new Time("35").getTime();
 
-   // ---------- PLAYER COORDINATES and COLLISION ---------- //
+// Update the count down every 1 second
+var x = setInterval(function() {
 
-//    let playerCoord = document.querySelector('playerGirl');
-//    let rect = elem.getBoundingClientRect();
-//    for (var key in rect) {
-//      if(typeof rect[key] !== 'function') {
-//        let makeCatcall = // choose catcaller audio function ;
-//        // play catcaller audio ;
-//      }
-//    }
+  var now = new Time().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;  
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("timer").innerHTML = seconds;
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+
+
+// -------------- AUDIO --------------- //
+  
+// WORKING CODE FOR AUDIO ////
+        // const whistle = new Audio("audio/wolf-whistle-14621.mp3");
+        // whistle.play("whistle"); 
+// END WORKING CODE FOR AUDIO ////
+
+
+// ------------
+// const areYouWorking = new Audio("working")
+
+// const whistle = new Audio("audio/wolf-whistle-14621.mp3");
+// whistle.play();
+
+//// sample code
+//   audio.addEventListener('ended',function(){
+//         audio.src = "new url";
+//         audio.pause();
+//         audio.load();
+//         audio.play();
+//     });
